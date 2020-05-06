b1569dde8235cbc755861fc3ad4c32c3f1c930b2
adma <greenpunk@list.ru>
2020-05-06 15:30:48 +0300

    work with requests

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('band/<int:id>', band_list, name='band_list')
+    path('band/<int:id>', band_list, name='band_list'),
+    path('band_group/<int:id>', band_main, name='band_main'),
 
 
 ]
--- mysite/music/views.py
+++ mysite/music/views.py
@@ -9,6 +9,22 @@ def home(request):
 
 def band_list(request, id):
     band_list = Band.objects.get(id=id)
-    context = {'band_list':band_list}
+    list = Band.objects.filter(genre__id=id).all()
+    image = Images.objects.get(id=id)
+    context = {
+        'band_list':band_list,
+        'image':image,
+        'list':list,
+
+    }
     return render(request, 'music/band_list.html', context)
+
+def band_main(request, id):
+    band = Band.objects.get(id=id)
+    image = Images.objects.get(id=id)
+    context = {
+        'band':band,
+        'image':image
+    }
+    return render(request, 'music/band_main.html', context)
 
